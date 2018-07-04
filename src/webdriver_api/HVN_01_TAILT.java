31a382b9adbac49ab8231cfe00c9edf2bdbebab6
AlexX Le <alexsandro134@gmail.com>
2018-07-04 09:44:46 +0700

    Add to github first time

---
+++ src/webdriver_api/HVN_01_TAILT.java
@@ -0,0 +1,38 @@
+package webdriver_api;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HVN_01_TAILT {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		// System.setProperty("webdriver.chrome.driver",
+		// ".\\driver\\chromedriver.exe");
+		// driver = new ChromeDriver();
+
+		System.setProperty("webdriver.ie.driver",
+				".\\driver\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+	}
+
+	@Test
+	public void TC_01_ABC() {
+		driver.get("http://tinhte.vn");
+	}
+
+	@Test
+	public void TC_02_ACB() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
