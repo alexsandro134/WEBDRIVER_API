95838a6d086c9fb2a311b9ca8e459ac80097b59f
AlexX Le <alexsandro134@gmail.com>
2018-07-27 11:01:30 +0700

    update

---
+++ src/webdriver_api/Topic_05_UserInteraction.java
@@ -0,0 +1,7 @@
+package webdriver_api;
+
+public class Topic_05_UserInteraction {
+	public void test() {
+
+}
+}
---
+++ src/webdriver_api/Topic_05_UserInteraction_new.java
@@ -0,0 +1,33 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_05_UserInteraction_new {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);		
+	}
+
+	@Test
+	public void f() {
+		Actions action = new Actions(driver);
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
