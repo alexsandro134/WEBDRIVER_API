4395ad115bf6a85c973f5e5ada0763f9d41df971
AlexX Le <alexsandro134@gmail.com>
2018-08-07 10:49:22 +0700

    test

--- src/webdriver_api/Topic_05_UserInteraction_new.java
+++ src/webdriver_api/Topic_05_UserInteraction_new.java
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
